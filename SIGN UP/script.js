86e80f2a689d279a7d7d0739dc15a0ea117d15c8
mohad6 <omachiattah20@gmail.com>
2025-09-29 13:07:39 +0100

    Refactor comments in script.js to enhance clarity and maintainability of animation descriptions

--- SIGN UP/script.js
+++ SIGN UP/script.js
@@ -1,9 +1,19 @@
+/**
+ * description: This file contains the JavaScript code for the sign-up page.
+ * It uses the ScrollReveal library to create scroll animations for various elements on the page.
+ * The animations are configured to originate from the bottom and slide in from the left for the body container.
+ * The form container and submit button have delayed animations to create a staggered effect.
+ */
 const animationslides = {
   origin: "bottom",
   distance: "50px",
   duration: "1000",
 };
-
+/**
+ * description: The following code uses the ScrollReveal library to create scroll animations for various elements on the sign-up page.
+ * The animations are configured to originate from the bottom and slide in from the left for the body container.
+ * The form container and submit button have delayed animations to create a staggered effect.
+ */
 ScrollReveal().reveal(".body-container", {
   ...animationslides,
   origin: "left",
