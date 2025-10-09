fd3da8037e722b82784df1ad71eb95bc07d63219
mohad6 <omachiattah20@gmail.com>
2025-10-09 11:54:26 +0100

    add scripts.js to the sigup page

---
+++ SIGN UP/script.js
@@ -0,0 +1,46 @@
+const animationslides = {
+  origin: "bottom",
+  distance: "50px",
+  duration: "1000",
+};
+
+ScrollReveal().reveal(".eco-logo", {
+  ...animationslides,
+  origin: "top",
+});
+ScrollReveal().reveal(".body-container", {
+  ...animationslides,
+  origin: "right",
+});
+ScrollReveal().reveal(".form-container", {
+  ...animationslides,
+  delay: 500,
+});
+ScrollReveal().reveal(".name", {
+  ...animationslides,
+  delay: 600,
+});
+ScrollReveal().reveal(".email", {
+  ...animationslides,
+  delay: 700,
+});
+ScrollReveal().reveal(".password", {
+  ...animationslides,
+  delay: 800,
+});
+ScrollReveal().reveal(".address", {
+  ...animationslides,
+  delay: 900,
+});
+ScrollReveal().reveal(".phone-number", {
+  ...animationslides,
+  delay: 1000,
+});
+ScrollReveal().reveal(".submit-btn", {
+  ...animationslides,
+  delay: 1500,
+});
+ScrollReveal().reveal(".signin-link", {
+  ...animationslides,
+  delay: 1800,
+});
